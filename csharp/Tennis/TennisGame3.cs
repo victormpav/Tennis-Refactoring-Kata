e4690dcc1f5380868f904837a3963d3298b0e931
Pavon Rufino <victor.pavon@hp.com>
2017-05-05 20:21:06 +0200

    remove magic strings

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -2,6 +2,16 @@ namespace Tennis
 {
     public class TennisGame3 : ITennisGame
     {
+        private const string LOVE_SCORE = "Love";
+        private const string FIFTEEN_SCORE = "Fifteen";
+        private const string THIRTY_SCORE = "Thirty";
+        private const string FORTY_SCORE = "Forty";
+        private const string ALL_SCORE = "-All";
+        private const string SOCORE_SEPARATOR = "-";
+        private const string DEUCE_SCORE = "Deuce";
+        private const string ADVANTAGE_SCORE = "Advantage ";
+        private const string WINER_MATCH = "Win for ";
+        private const string PLAYER_ONE_NAME = "player1";
         private int p2;
         private int p1;
         private string p1N;
@@ -18,22 +28,22 @@ namespace Tennis
             string s;
             if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6))
             {
-                string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
+                string[] p = { LOVE_SCORE, FIFTEEN_SCORE, THIRTY_SCORE, FORTY_SCORE };
                 s = p[p1];
-                return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+                return (p1 == p2) ? s + ALL_SCORE : s + SOCORE_SEPARATOR + p[p2];
             }
             else
             {
                 if (p1 == p2)
-                    return "Deuce";
+                    return DEUCE_SCORE;
                 s = p1 > p2 ? p1N : p2N;
-                return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
+                return ((p1 - p2) * (p1 - p2) == 1) ? ADVANTAGE_SCORE + s : WINER_MATCH + s;
             }
         }
 
         public void WonPoint(string playerName)
         {
-            if (playerName == "player1")
+            if (playerName == PLAYER_ONE_NAME)
                 this.p1 += 1;
             else
                 this.p2 += 1;
