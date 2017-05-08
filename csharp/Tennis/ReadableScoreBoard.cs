8fd1745c9579476722f30590230d44ec317b827c
Pavon Rufino <victor.pavon@hp.com>
2017-05-08 23:01:49 +0200

    Create ReadalbelScoreBoard

---
+++ csharp/Tennis/ReadableScoreBoard.cs
@@ -0,0 +1,17 @@
+﻿namespace Tennis
+{
+    public class ReadableScoreBoard
+    {
+        private Match match;
+
+        public ReadableScoreBoard(Match match)
+        {
+            this.match = match;
+        }
+
+        public string GetScore()
+        {
+            return string.Empty;
+        }
+    }
+}
